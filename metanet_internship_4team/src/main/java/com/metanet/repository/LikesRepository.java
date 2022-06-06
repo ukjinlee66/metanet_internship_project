9887bb04b3edac3fdc377c0d6442f83a9f1c9fc0
Juwoong <wwong7247@gmail.com>
2022-06-06 15:02:02 +0900

    마이페이지 Views, Likews, Save 관련 기능 구현 추가
    마이페이지 문의 , 포인트 쪽을 제외한   시청영상, 좋아요 영상,  다시볼 저장영상에 대한  기능을 구현하고 추가했습니다.

--- metanet_internship_4team/src/main/java/com/metanet/repository/LikesRepository.java
+++ metanet_internship_4team/src/main/java/com/metanet/repository/LikesRepository.java
@@ -1,5 +1,7 @@
 package com.metanet.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,9 @@ public interface LikesRepository extends JpaRepository< Likes, Integer>
 
 {
 
+	List<Likes> findByUsersNumber(int usersNumber);
+
+	int deleteByUsersNumberAndVideoNumber (int usersNumber,int videoNumber );
+		
+	
 }
--- metanet_internship_4team/src/main/java/com/metanet/repository/VideoRepository.java
+++ metanet_internship_4team/src/main/java/com/metanet/repository/VideoRepository.java
@@ -1,5 +1,8 @@
 package com.metanet.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +12,22 @@ import com.metanet.domain.Video;
 public interface VideoRepository extends JpaRepository <Video, Integer>
 {
 
+	List<Video> findByvideoTitleIsContaining(String videoTitle);
+	
 	Video findByvideoNumber(int videoNumber);
+	
+	//Optional<Video> findVideoByvideoTitleOrderByvideoViewDesc(String videoTitle); // 조회순 정렬 검색
+	
+	// 내가 추가한 내용 
+	
+	Optional<Video> findByVideoNumberAndRecipeKind (int videoNumber, String recipeKind);
 
+	List<Video> findByRecipeLevel(String recipeLevel);
+
+	List<Video> findByRecipeKindAndRecipeLevel(String UserReckind, String recipeLevel);
+	
+	Optional<Video> findByVideoName(String videoName);
+
+	
+	
 }
