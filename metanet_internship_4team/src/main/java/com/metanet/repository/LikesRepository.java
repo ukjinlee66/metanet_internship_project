11080fffbc939281e09809430e098e61296e1175
Youkjin Lee <ukjinlee66@gmail.com>
2022-06-10 09:42:54 +0900

    backend update

--- metanet_internship_4team/src/main/java/com/metanet/repository/LikesRepository.java
+++ metanet_internship_4team/src/main/java/com/metanet/repository/LikesRepository.java
@@ -1,8 +1,6 @@
 package com.metanet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.metanet.domain.Likes;
@@ -11,7 +9,7 @@ import com.metanet.domain.Likes;
 public interface LikesRepository extends JpaRepository< Likes, Long>
 {
 	int countByvideoNumber(int videoNumber);
-
+	boolean existsByvideoNumber(int videoNumber);
 //	@Query("SELECT COUNT(l.videoNumber)>0"+
 //			"FROM LIKES l" +
 //			"WHERE l.videoNumber =:videoNumber")
--- metanet_internship_4team/src/main/java/com/metanet/repository/VideoRepository.java
+++ metanet_internship_4team/src/main/java/com/metanet/repository/VideoRepository.java
@@ -11,6 +11,8 @@ import com.metanet.domain.Video;
 public interface VideoRepository extends JpaRepository <Video, String>
 {
 	List<Video> findByvideoTitleIsContaining(String videoTitle);
+	List<Video> findByrecipeKind(String recipeKind);
+	List<Video> findByrecipeLevel(String recipeLevel);
 	Video findByvideoNumber(int videoNumber);
 	//Optional<Video> findVideoByvideoTitleOrderByvideoViewDesc(String videoTitle); // 조회순 정렬 검색
 }
