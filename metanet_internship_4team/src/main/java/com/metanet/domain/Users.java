3925327faf5a75f48c70f49d37443250ccaf8c36
Juwoong <wwong7247@gmail.com>
2022-06-18 16:46:44 +0900

    add user domain column
    add user domain column

--- metanet_internship_4team/src/main/java/com/metanet/domain/Users.java
+++ metanet_internship_4team/src/main/java/com/metanet/domain/Users.java
@@ -61,6 +61,10 @@ public class Users
 	@Column(name = "USER_KIND")
 	private char userKind;
 	
+	@Column(name = "USER_ENDSUBSCRIBE")
+	private java.sql.Date userEndsubscribe;
+	
+	
 	@Column(name = "USER_DATE")
 	@NonNull
 	private java.sql.Date userDate;
