ffdb2e5583cab783f107cdc28869971e4fe2f407
Juwoong <wwong7247@gmail.com>
2022-06-20 08:53:19 +0900

    add test file for image hls lender

---
+++ streaming_server/iamgeTest/my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo  from './logo.svg';
+import Hello from './Hello';
+import Mono from './Mono';
+
+
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Mono />
+    </div>
+  );
+}
+
+
+
+export default App;
